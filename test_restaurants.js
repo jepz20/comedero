9493a47c90b16ea6051969bebbeb03065c8589fa
Jose Perdomo <jepz20@gmail.com>
2016-11-24 01:10:17 -0600

    template to add new restaurants temporaly disabled sw

---
+++ test_restaurants.js
@@ -0,0 +1,37 @@
+{
+  "address": {
+    "city": "Tegucigalpa",
+    "country": "Honduras",
+    "state": "Francisco Morazan",
+    "streetAddress": "Colonia Kennedy Segunda Entrada",
+    "zipCode": 504
+  },
+  "average_price": 30,
+  "categories": [
+    "Tipica",
+    "Baleada",
+    "Calle"
+  ],
+  "featureMenu": [
+    {
+      "description": "Deliciosa tortilla de harina con carne al gusto, mantequilla y nuestro encurtido especial",
+      "dish": "Baleada Especial",
+      "originalPrice": 28,
+      "price": 26
+    },
+    {
+      "description": "Deliciosa tortilla de harina con queso y mantequilla",
+      "dish": "Queso y Mantequilla",
+      "originalPrice": 18,
+      "price": 17
+    }
+  ],
+  "high_price": 28,
+  "image": "https://s3-media4.fl.yelpcdn.com/bphoto/sCN_ngCkL5YifvWm05Gjzg/o.jpg",
+  "key": 1,
+  "low_price": 17,
+  "name": "Baleadas Kennedy",
+  "open_hours": "9:00am - 9:00pm",
+  "rating": 4,
+  "website": "www.baleadaskennedy.hn"
+}
