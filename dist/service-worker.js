ca09d5bc021785cd48519eb95e50f74f7be7fe56
Jose Perdomo <jepz20@gmail.com>
2016-11-27 21:33:41 -0600

    rebuild

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["app.91ce8c65c1dabbfd364f.js","12b02ccc7cf2d0e0940bc4c0c34082b7"],["images/favicon.ico","c2b2f4b5dbb35fe83756dcf18f181a69"],["index.html","1a5ccf09ebd036fc34eb4b265ec538a3"],["manifest.a2ff6af39ed604da84b8.js","9ab56958a8ea110208861a2d673ed9f0"],["vendor.b7e6436d01c983b5e28f.js","0b1e1736ba3be69f9a630421ac275ab3"]];
+var PrecacheConfig = [["app.91ce8c65c1dabbfd364f.js","12b02ccc7cf2d0e0940bc4c0c34082b7"],["images/favicon.ico","c2b2f4b5dbb35fe83756dcf18f181a69"],["index.html","1296928fc40e739c759ecc46eba4cb0b"],["manifest.151cd8c6d704f2ba8e6d.js","c1aa3d21b86d7a87ae15d0455bf2050e"],["vendor.4d269f0156b3fa294cb0.js","0e8b670b82bdf8929718a304628a46bd"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1-comedero-' + (self.registration ? self.registration.scope : '') + '-';
 
